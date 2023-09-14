0acd66ed411f4eeb4e4edd73c673855576a811d4
pktatari <tawhidalmuhaimin@gmail.com>
2023-09-14 19:32:14 +0600

    tailwind setup complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1 className='text-cyan-400'>Vite + React</h1>
     </>
   )
 }
