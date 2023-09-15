a9cc6ba047f2e6f8b44dbb4d8ec756c5c5be0f85
pktatari <tawhidalmuhaimin@gmail.com>
2023-09-15 15:13:28 +0600

    Dynamic cart with credit limit exceed checking done

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,23 @@ import { useState } from 'react'
 
 function App() {
 const [cart,setCart] = useState([]);
+const [creditRemaining, setCreditRemaining ] = useState(20);
+const [creditTaken,setCreditTaken] = useState(0);
+const [price,setPrice] = useState(0);
 
 const handleSelect = (item) => {
-  const exists = cart.find((i)=> i===item)
+  if(creditTaken + item.credit_hours > 20){
+    return alert("credit_hours exedeed")
+  }
+
+  const exists = cart.find((i)=> i===item);
   if(exists){
     return alert("taken")
   }
   setCart([...cart, item]);
+  setCreditTaken(creditTaken + item.credit_hours);
+  setCreditRemaining(creditRemaining - item.credit_hours);
+  setPrice(price + item.price);
 }
 
   return (
@@ -19,16 +29,21 @@ const handleSelect = (item) => {
       <h1 className='text-black text-[32px] font-bold text-center mt-[60px]'>Course Registration</h1>
       
       {/* container */}
-      <div className='flex flex-wrap'>
+      <div className='flex'>
           {/* cards container */}
           <div>
-            <Courses handleSelect={handleSelect}></Courses>
+            <Courses handleSelect={handleSelect} ></Courses>
           </div>
 
 
           {/* cart */}
           <div>
-        <Cart cart={cart}></Cart>
+        <Cart 
+        cart={cart} 
+        creditTaken={creditTaken} 
+        creditRemaining={creditRemaining}
+        price = {price}
+        ></Cart>
           </div>
       </div>
     </div>
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
-const Cart = ({cart}) => {
-
+const Cart = ({cart, creditTaken, creditRemaining, price}) => {
+    console.log(creditRemaining, creditTaken);
     return (
         <div className="max-w-[312px] p-4 flex flex-col justify-between bg-white rounded-xl text-[#1c1b1b99] m-8">
-            <h3 className="text-lg font-bold text-[#2F80ED] pb-4">Credit Hour Remaining 7 hr</h3>
+            <h3 className="text-lg font-bold text-[#2F80ED] pb-4">Credit Hour Remaining {creditRemaining} hr</h3>
             <hr />
             <h2 className="font-bold text-black text-xl pt-4">Course Name</h2>
             <ol className="py-5">
@@ -12,16 +12,18 @@ const Cart = ({cart}) => {
                 }
             </ol>
             <hr />
-            <h3 className=" py-4 font-semibold">Total Credit Hour : 13</h3>
+            <h3 className=" py-4 font-semibold">Total Credit Hour : {creditTaken}</h3>
             <hr />
-            <h3 className="font-bold py-4">Total Price: 48000 USD</h3>
+            <h3 className="font-bold py-4">Total Price: {price} USD</h3>
 
         </div>
     );
 };
 
 Cart.propTypes = {
-    cart: PropTypes.array
+    cart: PropTypes.array,
+    creditRemaining: PropTypes.number,
+    creditTaken : PropTypes.number
   };
 
 export default Cart;
